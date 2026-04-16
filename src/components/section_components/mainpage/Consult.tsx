2863e1584c101e4b66f0ebfabf419be632a48bfc
Han <rayhan.dita45@smk.belajar.id>
2026-04-16 16:17:06 +0700

    scale up consult image on desktop monitor

--- src/components/section_components/mainpage/Consult.tsx
+++ src/components/section_components/mainpage/Consult.tsx
@@ -8,10 +8,10 @@ let Consult = function () {
           <div className="hero-deco hidden md:block"></div>
           <div className="hero-img-wrap flex-shrink-0 w-full md:w-auto flex justify-end items-end relative z-0 md:z-10 pointer-events-none">
             <div className="hero-person-placeholder relative h-[120px] md:h-[320px] overflow-visible">
-              <img
-                className="w-full h-auto pointer-events-none scale-[0.65] md:scale-[0.65] origin-bottom translate-x-[5.5rem] -translate-y-2 md:translate-x-0 md:translate-y-4 transition-transform duration-300"
-                src="/assets/img/Consult.png"
-              />
+                <img
+                  className="w-full h-auto pointer-events-none scale-[0.65] md:scale-[0.65] lg:scale-[0.85] xl:scale-100 origin-bottom translate-x-[5.5rem] -translate-y-2 md:translate-x-0 md:translate-y-4 lg:translate-y-0 transition-transform duration-300"
+                  src="/assets/img/Consult.png"
+                />
             </div>
           </div>
 
