e0b1a78e13a9c379f0cae2aac3206dbe6b90ccd3
Han <rayhan.dita45@smk.belajar.id>
2026-04-17 23:37:00 +0700

    style: increase button width in PUIThumbs

--- src/components/section_components/mainpage/PUIThumbs.tsx
+++ src/components/section_components/mainpage/PUIThumbs.tsx
@@ -10,8 +10,8 @@ let PUIThumbs = function () {
                     </p>
                 </div>
                 <div className="flex justify-center">
-                    <a href="/artikel" className="mb-12 md:mb-16 p-2 text-sm font-bold text-[#007F6D] border-2 w-[230px] rounded-lg border-[#007F6D] cursor-pointer hover:bg-[#007F6D] hover:text-white hover:scale-105 transition-all duration-300 flex items-center justify-center">
-                        Lihat Selengkapnya Tentang PUI
+                    <a href="/artikel" className="mb-12 md:mb-16 p-2 text-sm font-bold text-[#007F6D] border-2 w-[300px] rounded-lg border-[#007F6D] cursor-pointer hover:bg-[#007F6D] hover:text-white hover:scale-105 transition-all duration-300 flex items-center justify-center">
+                        Pelajari Selengkapnya Tentang PUI
                     </a>
                 </div>
                 <div className="flex justify-center -mb-8 md:-mb-16">
