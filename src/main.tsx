7615f5b6fca00c016f3af6163fc9efa02d5e973e
Han <rayhan.dita45@smk.belajar.id>
2026-04-23 09:18:16 +0700

    fix: scope global h1 style causing layout issues in tooltip

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx' // Impor komponen utama
+import './index.css'
 
 // Merender komponen App ke dalam elemen HTML dengan id="root"
 ReactDOM.createRoot(document.getElementById('root')!).render(
