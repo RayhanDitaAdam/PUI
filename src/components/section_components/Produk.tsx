c21efca54c2a9250ea064804a8773bceac0c84a7
Han <rayhan.dita45@smk.belajar.id>
2026-05-09 20:13:23 +0700

    Sync product page consult section and styling with mainpage

--- src/components/section_components/Produk.tsx
+++ src/components/section_components/Produk.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Footer from "../__global/Footer";
 import Header from "../__global/Header";
-import { Consult } from "./produk/Consult";
+import { Consult } from "./mainpage/Consult";
 import { ItemContext } from "./produk/ItemContext";
 import { Kriteria } from "./produk/Kriteria";
 import { MainSection } from "./produk/MainSection";
 import { FAQ } from "./mainpage/FAQ";
-import "./produk/style/init.css"
+import "./mainpage/style/init.css";
+import "./mainpage/style/section-cards.css";
+import "./mainpage/style/product.css";
 import gsap from 'gsap';
 // @ts-ignore
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
@@ -30,14 +32,22 @@ let Produk = function () {
     }, []);
 
     return (<>
-        <div className="bg-gray-200">
+        <div className="overflow-x-hidden text-white" style={{ background: "linear-gradient(135deg, #003D35, #091D1A)" }}>
             <Header />
             <div className="mt-10">
                 <MainSection />
-                <ItemContext />
-                <Kriteria />
-                <Consult />
-                <FAQ />
+                <div className="gsap-reveal">
+                    <ItemContext />
+                </div>
+                <div className="gsap-reveal">
+                    <Kriteria />
+                </div>
+                <div className="gsap-reveal">
+                    <Consult />
+                </div>
+                <div className="gsap-reveal">
+                    <FAQ />
+                </div>
                 <Footer />
             </div>
         </div>
